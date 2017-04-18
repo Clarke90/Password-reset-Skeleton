4f5bd0d8c5679662fb3ed7166770717251a95091
ChadClark_Dev <chadclarke@bell.net>
2017-04-18 15:54:31 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session')
+var mongoose = require('mongoose');
+var nodemailer = require('nodemailer');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bcrypt = require('bcrypt-nodejs');
+var async = require('async');
+var crypto = require('crypto');
+var flash = require('express-flash');
+
+// Its start coding!
+
+
+
+
+
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+module.exports = app;
